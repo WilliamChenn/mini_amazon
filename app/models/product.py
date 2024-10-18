21fa694054a20b94352c6ab1433eb134978fde52
prashamsakoirala <pkoirala04@gmail.com>
2024-10-18 15:55:17 -0400

    updated my branch

--- app/models/product.py
+++ app/models/product.py
@@ -1,29 +1,39 @@
 from flask import current_app as app
 
 
+
+
 class Product:
-    def __init__(self, id, name, price, available):
-        self.id = id
-        self.name = name
-        self.price = price
-        self.available = available
+   def __init__(self, id, name, price, available, seller_id, category_id, summary, created_at):
+       self.id = id
+       self.name = name
+       self.price = price
+       self.available = available
+       self.seller_id = seller_id
+       self.category_id = category_id
+       self.summary = summary
+       self.created_at = created_at
 
-    @staticmethod
-    def get(id):
-        rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE id = :id
-''',
-                              id=id)
-        return Product(*(rows[0])) if rows is not None else None
 
-    @staticmethod
-    def get_all(available=True):
-        rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE available = :available
-''',
-                              available=available)
-        return [Product(*row) for row in rows]
+#implement method for removing a product
+      
+   @staticmethod
+   def get(id):
+       rows = app.db.execute('''
+   SELECT id, name, price, available, seller_id, category_id, summary, created_at
+   FROM Products
+   WHERE id = :id
+   ''',
+                             id=id)
+       return Product(*(rows[0])) if rows is not None else None
+
+
+   @staticmethod
+   def get_all(available=True):
+       rows = app.db.execute('''
+   SELECT id, name, price, available
+   FROM Products
+   WHERE available = :available
+   ''',
+                             available=available)
+       return [Product(*row) for row in rows]
