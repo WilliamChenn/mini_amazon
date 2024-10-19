19ed735a1a920fbf0101b0995b06e9b502cb3e62
William <wzc2@duke.edu>
2024-10-19 12:12:07 -0400

    updated some id names so it specifies so like product_id, instaed of id

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -1,5 +1,5 @@
 from flask import current_app as app
-
+from datetime import datetime
 
 class Purchase:
     def __init__(self, purchase_id, user_id, product_id, time_purchased):
@@ -11,19 +11,48 @@ class Purchase:
     @staticmethod
     def get(purchase_id):
         rows = app.db.execute('''
-SELECT purchase_id, user_id, product_id, time_purchased
-FROM Purchases
-WHERE purchase_id = :purchase_id
-''',
-                              purchase_id=purchase_id)
+            SELECT purchase_id, user_id, product_id, time_purchased
+            FROM Purchases
+            WHERE purchase_id = :purchase_id
+        ''', purchase_id=purchase_id)
         return Purchase(*rows[0]) if rows else None
 
     @staticmethod
     def get_all_for_user(user_id):
         rows = app.db.execute('''
-SELECT purchase_id, user_id, product_id, time_purchased
-FROM Purchases
-WHERE user_id = :user_id
-''',
-                              user_id=user_id)
+            SELECT purchase_id, user_id, product_id, time_purchased
+            FROM Purchases
+            WHERE user_id = :user_id
+        ''', user_id=user_id)
+        return [Purchase(*row) for row in rows]
+
+    @staticmethod
+    def get_all_by_uid_since(uid, since):
+        """
+        Retrieves all purchases made by a user with user_id=uid since the specified datetime.
+
+        Parameters:
+            uid (int): The user ID.
+            since (str or datetime): The datetime from which to retrieve purchases. 
+                                      Can be a string in 'YYYY-MM-DD HH:MM:SS' format or a datetime object.
+
+        Returns:
+            List[Purchase]: A list of Purchase objects matching the criteria.
+        """
+        # Ensure 'since' is a string in the correct format
+        if isinstance(since, datetime):
+            since_str = since.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(since, str):
+            # Optionally, validate the string format here
+            since_str = since
+        else:
+            raise ValueError("Parameter 'since' must be a datetime object or a string in 'YYYY-MM-DD HH:MM:SS' format.")
+
+        rows = app.db.execute('''
+            SELECT purchase_id, user_id, product_id, time_purchased
+            FROM Purchases
+            WHERE user_id = :user_id AND time_purchased >= :since
+            ORDER BY time_purchased DESC
+        ''', user_id=uid, since=since_str)
+
         return [Purchase(*row) for row in rows]
