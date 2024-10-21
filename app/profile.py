601e67f0296441d8745496cbedf2cfb96bc35952
William <wzc2@duke.edu>
2024-10-21 17:28:12 -0400

    added profile order's

--- app/profile.py
+++ app/profile.py
@@ -1,20 +1,51 @@
 from flask import Blueprint, jsonify, render_template, request
 from flask_login import current_user
+import datetime
+
+from .models.product import Product
+#from .models.purchase import Purchase
+from .models.orders import Order  # Updated import
+from .models.order_items import OrderItem
 
 from app.models.reviews import Reviews
 
 
-bp = Blueprint('social', __name__)
+bp = Blueprint('profile', __name__)
 
 @bp.route('/profile', methods=['GET'])
-def recent_reviews():
-    
+def profile():
     if current_user.is_authenticated:
+        # Get recent reviews
         reviews = Reviews.get_top_by_user_id(current_user.id)
+        
+        # Get the user's orders
+        orders = Order.get_by_user(current_user.id)
+        
+        # For each order, get the associated order items and product details
+        orders_with_items = []
+        for order in orders:
+            order_items = OrderItem.get_by_order(order.order_id)
+            # For each order item, get product details
+            items_with_details = []
+            for item in order_items:
+                product = Product.get(item.product_id)
+                items_with_details.append({
+                    'order_item': item,
+                    'product': product
+                })
+            orders_with_items.append({
+                'order': order,
+                'items': items_with_details
+            })
     else:
         reviews = None
-    
-    return render_template('profile.html', reviews=reviews)
+        orders_with_items = None
+
+    return render_template('profile.html', reviews=reviews, orders_with_items=orders_with_items)
+
+
+
+
 
 @bp.route('/add-review', methods=['POST'])
 def add_review():
